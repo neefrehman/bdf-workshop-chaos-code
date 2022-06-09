5f93226faa2e3910b0028a8a284b3646851d9770
Neef Rehman <neef.rehman@gmail.com>
2022-06-09 23:04:57 +0100

    complete sketch

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import {
   type BezierCurvePoints,
   bezierCurveBetween,
   clearBackgroundWithColor,
+  generateTextPath,
 } from "utils/canvas2d";
-import { getDistance, type Vector } from "utils/math";
+import { getDistance, lerpVector, mapToRange, type Vector } from "utils/math";
 import { getShortestViewportDimension } from "utils/math";
-import { pick } from "utils/random";
+import { inRange, pick, simplex1D, inGaussian } from "utils/random";
 
 class NoisePoint {
   ctx: CanvasRenderingContext2D;
@@ -20,16 +21,16 @@ class NoisePoint {
 
   constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx;
-    this.xOff = 0;
-    this.yOff = 0;
-    this.v = 0.005;
+    this.xOff = inRange(200);
+    this.yOff = inRange(400, 600);
+    this.v = inGaussian(0.005, 0.001);
   }
 
   move = (): void => {
     this.xOff += this.v;
     this.yOff += this.v;
-    this.x = 1;
-    this.y = 1;
+    this.x = mapToRange(simplex1D(this.xOff), -1, 1, 0, this.ctx.canvas.width / 2);
+    this.y = mapToRange(simplex1D(this.yOff), -1, 1, 0, this.ctx.canvas.height / 2);
   };
 
   drawPoint = (): void => {
@@ -49,12 +50,19 @@ class NoisePoint {
 
   getBezierCurvePoints = (nearestPoint: Vector<2>): BezierCurvePoints => {
     const [x, y] = nearestPoint;
-    return [
-      [this.x, this.y],
-      [this.x, this.y],
-      [x, y],
-      [x, y],
+    const nX = x + inRange(2);
+    const nY = y + inRange(2);
+    const vectorNearPoint: Vector<2> = [
+      this.x + simplex1D(this.x) * 5,
+      this.y + simplex1D(this.y) * 5,
     ];
+    const vectorNearPath: Vector<2> = [
+      nX + simplex1D(nX) * 50,
+      nY + simplex1D(nY) * 50,
+    ];
+    const cp1 = lerpVector(vectorNearPoint, vectorNearPath, inGaussian(0.33, 0.1));
+    const cp2 = lerpVector(vectorNearPoint, vectorNearPath, inGaussian(0.66, 0.1));
+    return [[this.x, this.y], cp1, cp2, [nX, nY]];
   };
 
   drawBezierCurve = (nearestPoint: Vector<2>, colour = "white"): void => {
@@ -77,11 +85,23 @@ const sketch: Canvas2DSetupFn = ({ ctx, width, height }) => {
   ctx.font = `${SCALE}px Helvetica`;
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
+  const textPath = generateTextPath(ctx, WORD, width / 2, height / 2);
+  const POINT_COUNT = inRange(8, 20, { isInteger: true });
+  const points: NoisePoint[] = [...Array(POINT_COUNT)].map(() => new NoisePoint(ctx));
 
   return ({}) => {
     clearBackgroundWithColor(ctx, backgroundColor);
     ctx.fillStyle = foregroundColor;
     ctx.fillText(WORD, width / 2, height / 2);
+
+    points.forEach(point => {
+      point.move();
+      point.drawPoint();
+      const nearestPointsOnTextPath = point.getNearestPointsFromPath(textPath);
+      nearestPointsOnTextPath.forEach(nearestPointOnTextPath => {
+        point.drawBezierCurve(nearestPointOnTextPath, pick(pallette));
+      });
+    });
   };
 };
 
